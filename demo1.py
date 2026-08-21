ddc98fbef908b3810cb084ea3edbcd7129a18602
rishabh7804 <rishabhmasram4@gmail.com>
2026-08-21 18:44:42 +0530

    Update files

---
+++ demo1.py
@@ -0,0 +1,11 @@
+def choice(areaa,perimeter):
+    if areaa==1:
+        r= int(input("enter radius"))
+        pi =3.14
+        a=pi*r**2
+        print(a)
+    if perimeter==1:
+        re=int(input("enter radi"))
+        pi=3.14
+        pr = 2*pi*re
+        print("perimeter",pr)
